07b80a979bc38d8f9a35fbe4797c8971beb9c75c
mohsinian <tahsinbd.ahmed1@gmail.com>
2024-04-23 02:08:45 +0600

    can toggle to done, add and delete from todo list

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let body = document.querySelector('body');
+let body = document.getElementById("body-field");
 //let's seed some initial data
-let toDo = [{task: "Go to School", taskStatus: "pending"}, {task: "Wake Up", taskStatus: "done"}];
+let toDo = [{task: "Go to School", taskStatus: "pending", id: Date.now()}, {task: "Wake Up", taskStatus: "done"}];
 viewToDo(toDo);
 function viewToDo(list) {
+    body.innerHTML="";
     const divBox = document.createElement('div');
     body.append(divBox);
+    let taskInputDiv = document.createElement('div');
     list.forEach(element => {
         const task = document.createElement('div');
+        const btn = document.createElement('button');
+        const delButton = document.createElement('button');
         task.style.display="flex";
         task.style.gap="15px";
         task.append(element.task);
         if(element.taskStatus=="pending") task.style.color="red";
         else task.style.color="green";
-        const btn = document.createElement('button');
         btn.textContent = "Done";
         task.append(btn);
+        delButton.textContent = "Delete";
+        task.append(delButton);
         divBox.append(task);
         btn.addEventListener('click',()=>{
             if(element.taskStatus=="pending") 
@@ -22,10 +27,14 @@ function viewToDo(list) {
                 element.taskStatus=="done";
                 task.style.color="green";
             }
-        })
+        });
+        
+        delButton.addEventListener('click',()=>{
+            del(element.id);
+            viewToDo(toDo);
+        });
     });
 
-let taskInputDiv = document.createElement('div');
 let taskInput = document.createElement('input');
 taskInputDiv.append(taskInput);
 const addBtn = document.createElement('button');
@@ -36,14 +45,18 @@ addBtn.addEventListener('click',()=>{
     console.log(taskInput.value);
     addToDo(taskInput.value);
     console.log(toDo);
-    divBox.innerHTML="";
-    taskInputDiv.innerHTML="";
     viewToDo(toDo);
 })
 }
 
+
+
 function addToDo(value) {
-    toDo.push({task: value, taskStatus: "pending"});
+    toDo.push({task: value, taskStatus: "pending", id: Date.now()});
+}
+
+function del(id) {
+    toDo = toDo.filter(toDo => toDo.id !== id)
 }
 
 
